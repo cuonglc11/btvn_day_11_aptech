8db9ea4b46d93cea04cb9595eca38a9289de3ec3
manhcuong <cuonglu603@gmail.com>
2025-03-10 00:26:23 +0700

    code

--- js/main.js
+++ js/main.js
@@ -3,66 +3,74 @@ const listProduct = [];
 let id = 0;
 const token = localStorage.getItem("token");
 if (!token) {
-	window.location = "/day9/login.html";
+  window.location = "/day9/login.html";
 }
 
-document.getElementById("formCategory").addEventListener("submit", function(e) {
-		e.preventDefault();
-		const data = {};
-		const form = new FormData(e.target);
-		form.forEach((item, key) => {
-			data["id"] = Math.floor(Math.random() * 10);
-			data[key] = item;
-		});
-		listcategoris.push(data);
-		loadCategory();
-		document.getElementById("formCategory").reset();
-	});
-document.getElementById("formProduct").addEventListener("submit", function(e) {
-	e.preventDefault();
-	const data = {};
-	const form = new FormData(e.target);
-	form.forEach((item, key) => {
-		data["id"] = Math.floor(Math.random() * 10);
-		data[key] = item;
-	});
-	listProduct.push(data);
-	document.getElementById("formProduct").reset();
-	loadProduct();
+document
+  .getElementById("formCategory")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = {};
+    const form = new FormData(e.target);
+    form.forEach((item, key) => {
+      data["id"] = Math.floor(Math.random() * 10);
+      data[key] = item;
+    });
+    listcategoris.push(data);
+    loadCategory();
+    document.getElementById("formCategory").reset();
+  });
+document.getElementById("formProduct").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = {};
+  const form = new FormData(e.target);
+  form.forEach((item, key) => {
+    data["id"] = Math.floor(Math.random() * 10);
+    data[key] = item;
+  });
+  listProduct.push(data);
+  document.getElementById("formProduct").reset();
+  loadProduct();
 });
-
+document.getElementById("LogOut").onclick = () => {
+  if (confirm("Are you sure you want to log out")) {
+    localStorage.removeItem("token");
+    window.location = "/day9/login.html";
+  } else {
+  }
+};
 function loadCategory() {
-	document.getElementById("category").innerHTML =
-		' <option value="">Category</option>';
-	listcategoris.forEach((item) => {
-		if (!document.getElementById(`category_${item.id}`)) {
-			document.getElementById("listCategoris").innerHTML += `
+  document.getElementById("category").innerHTML =
+    ' <option value="">Category</option>';
+  listcategoris.forEach((item) => {
+    if (!document.getElementById(`category_${item.id}`)) {
+      document.getElementById("listCategoris").innerHTML += `
         <div class="detail-category">
-          <div>
+          <div class="title-category">
             <p>${item.name}</p>
             <p>${item.discription}</p>
           </div>
           <div id="category_${item.id}" class="list-product"></div>
         </div>
       `;
-		}
-		document.getElementById("category").innerHTML += `
+    }
+    document.getElementById("category").innerHTML += `
         <option value="${item.id}">${item.name}</option>`;
-	});
+  });
 }
 
 function loadProduct() {
-	const categories = document.querySelectorAll("[id^='category_']");
-	categories.forEach((category) => {
-		category.innerHTML = "";
-	});
-	listProduct.forEach((item) => {
-		const category = document.getElementById(
-			"category_" + getCategory(item.category)
-		);
+  const categories = document.querySelectorAll("[id^='category_']");
+  categories.forEach((category) => {
+    category.innerHTML = "";
+  });
+  listProduct.forEach((item) => {
+    const category = document.getElementById(
+      "category_" + getCategory(item.category)
+    );
 
-		if (category) {
-			category.innerHTML += `
+    if (category) {
+      category.innerHTML += `
         <div class="product-item">
             <img src="${item.image}" alt="">
             <h1>
@@ -75,12 +83,13 @@ function loadProduct() {
              <p>${item.discription}</p>
         </div>
         `;
-		} else {}
-	});
+    } else {
+    }
+  });
 }
 
 function getCategory(idCategory) {
-	const category = listcategoris.find((c) => c.id === Number(idCategory));
-	return category ? category.id : "";
+  const category = listcategoris.find((c) => c.id === Number(idCategory));
+  return category ? category.id : "";
 }
 // jquery
